43d86b741f04304173fa822f6f3be4e557638531
suvojitsarkar <sarkarsuvojit17@gmail.com>
2025-08-13 19:41:06 +0530

    Create ORgate.py

---
+++ ORgate.py
@@ -0,0 +1,18 @@
+print('| INPUT | OUTPUT |')
+while True:    
+    a=int(input("enter input: "))
+    b=int(input("enter second input:"))
+    i=input("want to contnue (y/n): ")
+    
+    if (a==1 or a==0) and (b==1 or b==0):
+        
+        if a==1 or b==1:
+            print(f"| {a} | {b} | 1      |")
+        else:
+            print(f"| {a} | {b} | 0      |")
+        if i=='n':
+            break
+        else:
+            continue
+    else:
+        print("ENTRE BINARY INPUTS!")
